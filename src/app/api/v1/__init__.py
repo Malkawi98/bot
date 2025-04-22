4a14bc4077fe9e6f561485b5373badc71eee2dfd
Hamza Malkawi <malkawi98@yahoo.com>
2025-04-22 18:52:21 +0300

    fix the deps

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 from .login import router as login_router
 from .logout import router as logout_router
 # from app.api.v1.posts import router as posts_router
-from .tasks import router as tasks_router
 # from .tiers import router as tiers_router
 from .users import router as users_router
 from .rag import router as rag_router
@@ -14,7 +13,6 @@ router.include_router(login_router)
 router.include_router(logout_router)
 router.include_router(users_router)
 # router.include_router(posts_router)
-router.include_router(tasks_router)
 # router.include_router(tiers_router)
 router.include_router(rag_router)
 router.include_router(rag_ui_router)
--- src/app/core/worker/settings.py
+++
@@ -1,8 +0,0 @@
-from ...core.config import settings
-from .functions import sample_background_task, shutdown, startup
-
-class WorkerSettings:
-    functions = [sample_background_task]
-    on_startup = startup
-    on_shutdown = shutdown
-    handle_signals = False
